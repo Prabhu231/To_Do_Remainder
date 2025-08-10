5834e417a1344f758846f886dc30eb4d580ad94d
Prabhu Patil <prabhumahesh.patil@gmail.com>
2025-08-11 03:12:02 +0530

    final code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import cron from "node-cron";
 import sendMail from "./services/sendMail.js";
 
-const sendScheduledMail = async () => {
-  await sendMail();
-};
-
-sendScheduledMail();
+cron.schedule(
+  "0 8,14,18 * * *",
+  async () => {
+    try {
+      await sendMail();
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  {
+    timezone: "Asia/Kolkata",
+  }
+);
